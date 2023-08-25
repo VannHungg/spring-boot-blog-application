a734e50a9fe23008f818ae0a4caa0f42a6edb275
VannHungg <83432918+VannHungg@users.noreply.github.com>
2023-08-25 07:32:46 +0700

    fix requestmatches function

--- src/main/java/com/example/springbootblogapplication/config/WebSecurityConfig.java
+++ src/main/java/com/example/springbootblogapplication/config/WebSecurityConfig.java
@@ -1,30 +1,36 @@
 package com.example.springbootblogapplication.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+//@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
     public static final String[] WHITELIST = {
             "/"
     };
 
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->  auth
-                    .requestMatchers("/").permitAll()
+                    .requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll()
                     .anyRequest().authenticated()
-                );
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
